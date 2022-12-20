88b379eda5997d1b2bcfe0e7b3e0a8c6ee62b483
Matt Petters <mcpetters@gmail.com>
2022-12-20 18:21:03 -0500

    12 days of xmas

--- projects/control-flow/src/main.rs
+++ projects/control-flow/src/main.rs
@@ -15,6 +15,12 @@ fn main() {
     collection();
     collection_for();
     for_in_range();
+    println!(
+        "fahrenheit_to_celsius(212.0) = {}",
+        fahrenheit_to_celsius(212.0)
+    );
+    println!("fibonacci(10) = {}", fibonacci(10));
+    twelve_days_of_christmas();
 }
 
 fn loop_labels() {
@@ -75,4 +81,59 @@ fn for_in_range() {
         println!("{number}!");
     }
     println!("LIFTOFF!!!");
+}
+
+// convert fahrenheit to celsius
+fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
+    (fahrenheit - 32.0) * 5.0 / 9.0
+}
+
+// generate nth fibonacci number
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
+
+// print the lyrics of 12 days of christmas
+fn twelve_days_of_christmas() {
+    let days = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+    let gifts = [
+        "a partridge in a pear tree",
+        "two turtle doves",
+        "three french hens",
+        "four calling birds",
+        "five golden rings",
+        "six geese a-laying",
+        "seven swans a-swimming",
+        "eight maids a-milking",
+        "nine ladies dancing",
+        "ten lords a-leaping",
+        "eleven pipers piping",
+        "twelve drummers drumming",
+    ];
+
+    for i in 0..12 {
+        println!(
+            "On the {} day of Christmas my true love gave to me",
+            days[i]
+        );
+        for j in (0..i + 1).rev() {
+            if i == 0 {
+                println!("{}", gifts[j]);
+            } else if j == 0 {
+                println!("and {}", gifts[j]);
+            } else {
+                println!("{}", gifts[j]);
+            }
+        }
+        println!("");
+    }
 }
