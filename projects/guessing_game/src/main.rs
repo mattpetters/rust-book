23107d960ea7f77fd80509856b0958b4fe48bcb5
Matt Petters <mcpetters@gmail.com>
2022-12-20 16:23:33 -0500

    shadowing / type conversion

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,7 +1,13 @@
+use rand::Rng; // trait must be in scope for rand::thread_rng to work
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // 1..=100 is a range expression
+
+    println!("The secret number is {secret_number}");
 
     println!("Please input your guess.");
 
@@ -11,5 +17,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // shadowing is often used to convert a value from one type to another
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
