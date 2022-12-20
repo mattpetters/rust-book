133b3296f94385c7a59c7a60c9787fdcdecb1cb1
Matt Petters <mcpetters@gmail.com>
2022-12-20 17:46:23 -0500

    more functions

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    // another_function(3);
+    print_labeled_measurement(3, 'h');
+    expression_test();
 }
 
-fn another_function() {
-    println!("Another function.");
+fn print_labeled_measurement(value: i32, label: char) {
+    println!("The value is {} {}", value, label);
+}
+
+fn expression_test() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
 }
