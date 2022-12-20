304aadcdb6420669322583bc55719c320a95a178
Matt Petters <mcpetters@gmail.com>
2022-12-20 16:37:31 -0500

    finish ch 2

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -7,24 +7,32 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100); // 1..=100 is a range expression
 
-    println!("The secret number is {secret_number}");
+    // println!("The secret number is {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // shadowing is often used to convert a value from one type to another
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // shadowing is often used to convert a value from one type to another
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
