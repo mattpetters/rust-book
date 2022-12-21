e0b26acab4d26579cb690444fcd8196a14a7f219
Matt Petters <mcpetters@gmail.com>
2022-12-20 22:01:49 -0500

    end 6.3

--- projects/if-let/src/main.rs
+++ projects/if-let/src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("Hello, world!");
+    let config_max = Some(3u8);
+    // instead of this:
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (),
+    // }
+
+    //do this:
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    } else {
+        println!("The maximum is not configured");
+    }
+
+    //In other words, you can think of if let as syntax sugar for
+    //a match that runs code when the value matches one pattern
+    //and then ignores all other values.
+
+    // let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+    //     _ => count += 1,
+    // }
+
+    // vs.
+
+    // let mut count = 0;
+    // if let Coin::Quarter(state) = coin {
+    //     println!("State quarter from {:?}!", state);
+    // } else {
+    //     count += 1;
+    // }
 }
