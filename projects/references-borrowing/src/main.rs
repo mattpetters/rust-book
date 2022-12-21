75b280174bf98aa4f2f2c0373ca57091d3f638cb
Matt Petters <mcpetters@gmail.com>
2022-12-20 19:57:02 -0500

    refs + borrowing

--- projects/references-borrowing/src/main.rs
+++ projects/references-borrowing/src/main.rs
@@ -1,3 +1,42 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+
+    // change(&s);
+
+    {
+        let r1 = &mut s;
+        println!("{}", r1);
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
+
+    println!("{}", r2);
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+                 // let r3 = &mut s; // BIG PROBLEM
+
+    println!("{}, {}", r1, r2);
+
+    // let reference_to_nothing = dangle();
 }
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
+// fn dangle() -> String {
+//     let s = String::from("hello");
+
+//     s
+// }
