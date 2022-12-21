30c2496dc65d59bc7ddb94dc5df10798573b61e3
Matt Petters <mcpetters@gmail.com>
2022-12-20 21:55:45 -0500

    6.2 end

--- projects/match-control/src/main.rs
+++ projects/match-control/src/main.rs
@@ -28,6 +28,15 @@ enum Coin {
 fn main() {
     println!("Hello, world!");
     value_in_cents(Coin::Quarter(UsState::Alaska));
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -44,3 +53,15 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
     }
 }
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+fn reroll() {}
