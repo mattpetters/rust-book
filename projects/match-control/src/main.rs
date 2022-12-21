c80c35eb8fb66bf5d03fd95071103caf25e5f702
Matt Petters <mcpetters@gmail.com>
2022-12-20 21:49:51 -0500

    match control flowwww

---
+++ projects/match-control/src/main.rs
@@ -0,0 +1,46 @@
+// ENUM OF ALL 50 STATES
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+fn main() {
+    println!("Hello, world!");
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
