b77cfc5228a16db17e10b950120d2d610ab2b58d
Matt Petters <mcpetters@gmail.com>
2022-12-21 10:49:26 -0500

    begin 10.3

--- projects/generics/src/main.rs
+++ projects/generics/src/main.rs
@@ -1,70 +1,129 @@
-// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-//     let mut largest = &list[0];
+use std::fmt::Display;
 
-//     for item in list {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
+pub trait Summary {
+    fn summarize_author(&self) -> String;
 
-//     largest
-// }
-
-// fn main() {
-//     let number_list = vec![34, 50, 25, 100, 65];
-
-//     let result = largest(&number_list);
-//     println!("The largest number is {}", result);
-
-//     let char_list = vec!['y', 'm', 'a', 'q'];
-
-//     let result = largest(&char_list);
-//     println!("The largest char is {}", result);
-
-//     let p1 = Point { x: 5, y: 10 };
-//     let p2 = Point { x: 1.0, y: 4.0 };
-//     // let p3 = Point { x: "Hello", y: 'c' };
-//     let p = Point { x: 5, y: 10 };
-
-//     println!("p.x = {}", p.x());
-// }
-
-// struct Point<T> {
-//     x: T,
-//     y: T,
-// }
-
-// impl<T> Point<T> {
-//     fn x(&self) -> &T {
-//         &self.x
-//     }
-// }
-
-// impl Point<f32> {
-//     fn distance_from_origin(&self) -> f32 {
-//         (self.x.powi(2) + self.y.powi(2)).sqrt()
-//     }
-// }
-
-struct Point<X1, Y1> {
-    x: X1,
-    y: Y1,
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
 }
 
-impl<X1, Y1> Point<X1, Y1> {
-    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
-        Point {
-            x: self.x,
-            y: other.y,
-        }
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// fn some_function<T, U>(t: &T, u: &U) -> i32
+// where
+//     T: Display + Clone,
+//     U: Clone + Debug,
+// {
+// }
+
+//longer version
+// pub fn notify<T: Summary>(item: &T) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("dog_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
     }
 }
 
 fn main() {
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c' };
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
 
-    let p3 = p1.mixup(p2);
+    println!("1 new tweet\n {}", tweet.summarize());
 
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+
+    let tweet = returns_summarizable();
+    println!("1 new tweet\n {}", tweet.summarize());
 }
+
+//conditionally implement method based on presence of trait of underlying type
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+// fails to compile
+// fn returns_summarizable2(switch: bool) -> impl Summary {
+//     if switch {
+//         NewsArticle {
+//             headline: String::from("Penguins win the Stanley Cup Championship!"),
+//             location: String::from("Pittsburgh, PA, USA"),
+//             author: String::from("Iceburgh"),
+//             content: String::from(
+//                 "The Pittsburgh Penguins once again are the best \
+//                  hockey team in the NHL.",
+//             ),
+//         }
+//     } else {
+//         Tweet {
+//             username: String::from("horse_ebooks"),
+//             content: String::from("of course, as you probably already know, people"),
+//             reply: false,
+//             retweet: false,
+//         }
+//     }
+// }
