684c0c8dada6c19c8fb72de97618e0ea325336eb
Matt Petters <mcpetters@gmail.com>
2022-12-21 10:31:55 -0500

    moving on to traits

--- projects/generics/src/main.rs
+++ projects/generics/src/main.rs
@@ -1,22 +1,70 @@
-fn largest(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
 
-    for item in list {
-        if item > largest {
-            largest = item;
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest(&char_list);
+//     println!("The largest char is {}", result);
+
+//     let p1 = Point { x: 5, y: 10 };
+//     let p2 = Point { x: 1.0, y: 4.0 };
+//     // let p3 = Point { x: "Hello", y: 'c' };
+//     let p = Point { x: 5, y: 10 };
+
+//     println!("p.x = {}", p.x());
+// }
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+// }
+
+// impl Point<f32> {
+//     fn distance_from_origin(&self) -> f32 {
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
+
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
         }
     }
-
-    largest
 }
+
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let p3 = p1.mixup(p2);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
