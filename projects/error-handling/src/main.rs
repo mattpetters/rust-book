19548579c6eafb26ee4d40b29e9e4855632db17a
Matt Petters <mcpetters@gmail.com>
2022-12-21 08:29:23 -0500

    create file if doesn't exist

--- projects/error-handling/src/main.rs
+++ projects/error-handling/src/main.rs
@@ -1,3 +1,21 @@
+use std::{fs::File, io::ErrorKind};
 fn main() {
-    println!("Hello, world!");
+    // panic!("crash and burn");
+
+    // let v = vec![1, 2, 3];
+
+    // v[99];
+
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+            },
+            other_error => panic!("There was a problem opening the file: {:?}", other_error),
+        },
+    };
 }
