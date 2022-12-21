135416bd78d1a2cbbcffbb3f9ba0ed166e1c0070
Matt Petters <mcpetters@gmail.com>
2022-12-21 07:00:30 -0500

    end 8.2, init 8.3

--- projects/utf8-encoded-text/src/main.rs
+++ projects/utf8-encoded-text/src/main.rs
@@ -1,3 +1,73 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+
+    let s1 = String::from("hello");
+    // let h = s1[0]; // this will not work because strings are not indexed
+
+    let hello = String::from("Hola");
+
+    let hello = String::from("Здравствуйте");
+
+    let hello = "Здравствуйте";
+    // let answer = &hello[0]; // this will not work because strings are not indexed
+    // the first byte is 208, the second is 151, and the third is 189
+
+    let hello = "Здравствуйте";
+
+    let s = &hello[0..2];
+
+    println!("s is {s}");
+
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
 }
