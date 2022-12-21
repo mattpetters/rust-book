a0deb471cb686bad65ccacfc3e6c003dd2c5ce54
Matt Petters <mcpetters@gmail.com>
2022-12-20 20:51:10 -0500

    moving on to method syntax

--- projects/structs-ex-program/src/main.rs
+++ projects/structs-ex-program/src/main.rs
@@ -1,12 +1,46 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
+
+    println!("rect1 is {} {}", rect1.width, rect1.height);
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    // {:?} is for debug
+    // {:#?} is for pretty debug
+    println!("rect2 is {:#?}", rect2);
+
+    let scale = 3;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
