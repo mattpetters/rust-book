a9eb0103b6073df7d90d36e5358ff90673cb1b03
Matt Petters <mcpetters@gmail.com>
2022-12-20 20:15:04 -0500

    array slices etc

--- projects/slices/src/main.rs
+++ projects/slices/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let s = String::from("Testing a string");
-    let f_word = first_word_index(&s);
+    let f_word = first_word(&s);
     println!("The first word is {}", f_word);
 
     let s = String::from("hello world");
@@ -9,10 +9,35 @@ fn main() {
     let world = &s[6..11];
 
     println!("{} {}", hello, world);
+
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
 }
 
 // &str is a string slice
-fn first_word_index(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
