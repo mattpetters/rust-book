9be709ddb0f064cf95be47ec694d5b17cb775577
Matt Petters <mcpetters@gmail.com>
2022-12-20 20:57:46 -0500

    moving on to can hold

--- projects/structs-ex-program/src/main.rs
+++ projects/structs-ex-program/src/main.rs
@@ -18,29 +18,6 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-
-    println!("rect1 is {} {}", rect1.width, rect1.height);
-
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-
-    // {:?} is for debug
-    // {:#?} is for pretty debug
-    println!("rect2 is {:#?}", rect2);
-
-    let scale = 3;
-    let rect1 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
-
-    dbg!(&rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
