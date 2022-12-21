3ff8152964738b524ae31854e4eb250e61a99cc3
Matt Petters <mcpetters@gmail.com>
2022-12-21 08:32:30 -0500

    closure version of file open and create

--- projects/error-handling/src/main.rs
+++ projects/error-handling/src/main.rs
@@ -8,14 +8,25 @@ fn main() {
 
     let greeting_file_result = File::open("hello.txt");
 
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
-            },
-            other_error => panic!("There was a problem opening the file: {:?}", other_error),
-        },
-    };
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+    //         },
+    //         other_error => panic!("There was a problem opening the file: {:?}", other_error),
+    //     },
+    // };
+
+    // come back after reading chapter 13
+    let greeting_file = File::open("test.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("test.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
 }
