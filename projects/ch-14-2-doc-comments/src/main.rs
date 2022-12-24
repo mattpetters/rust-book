b22524652f4d160f0870226c413dd3eb2686a830
Matt Petters <mcpetters@gmail.com>
2022-12-24 10:21:28 -0500

    doctests

---
+++ projects/ch-14-2-doc-comments/src/main.rs
@@ -0,0 +1,14 @@
+/// This is a doc comment.
+///
+/// # Examples
+/// ```
+/// let x = 5;
+/// let y = my_crate::add_one(x);
+/// assert_eq!(y, 6);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+fn main() {
+    println!("Hello, world!");
+}
---
+++ projects/test-comments/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// use test_comments::add_one;
+///
+/// let arg = 5;
+/// let answer = add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
