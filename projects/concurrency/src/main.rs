49d3c6318c945791e6dc116c4ef4ec41050a1fa2
Matt Petters <mcpetters@gmail.com>
2022-12-24 22:44:09 -0500

    basic threading

--- projects/concurrency/src/main.rs
+++ projects/concurrency/src/main.rs
@@ -1,3 +1,27 @@
+use std::{thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
 }
+
+// “Do not communicate by sharing memory; instead, share memory by communicating.”
